42fe99d1928ef93027d1e9f1dda9d0967586a605
rszaimov <radoslavzaimov@gmail.com>
2026-02-10 12:21:40 +0200

    feat: add more tests for the searchService

--- tests/Unit/SearchServiceTest.php
+++ tests/Unit/SearchServiceTest.php
@@ -80,5 +80,57 @@ class SearchServiceTest extends TestCase
         $this->assertEquals(0, $result['total_pages']);
         $this->assertFalse($result['has_more']);
     }
-   
+
+    public function test_search_returns_correct_page_number()
+    {
+        $result = $this->searchService->search('sneakers', [], 3, null, 20);
+
+        $this->assertEquals(3, $result['current_page']);
+    }
+
+    public function test_different_page_sizes()
+    {
+        $result10 = $this->searchService->search('sneakers', [], 1, null, 10);
+        $result50 = $this->searchService->search('sneakers', [], 1, null, 50);
+
+        $this->assertLessThanOrEqual(10, count($result10['data']));
+        $this->assertLessThanOrEqual(50, count($result50['data']));
+        $this->assertEquals(10, $result10['per_page']);
+        $this->assertEquals(50, $result50['per_page']);
+    }
+
+    public function test_applies_per_page_brand_cap()
+    {
+        // This test would work best with a separate test DB and seeded test data
+        $brandLimits = \App\Models\Brand::whereNotNull('ad_limit')
+                ->pluck('ad_limit', 'id')
+                ->toArray();
+        $globalBrandLimit = config('search.brand_limit', 3);
+        
+        $result1 = $this->searchService->search('shoes', [], 1, null, 20);
+        $result2 = $this->searchService->search('shoes', [], 2, null, 20);
+
+        // Count ads per brand
+        $brandCounts1 = [];
+        foreach ($result1['data'] as $ad) {
+            $brandId = $ad['brand_id'];
+            $brandCounts1[$brandId] = ($brandCounts1[$brandId] ?? 0) + 1;
+        }
+        $brandCounts2 = [];
+        foreach ($result2['data'] as $ad) {
+            $brandId = $ad['brand_id'];
+            $brandCounts2[$brandId] = ($brandCounts2[$brandId] ?? 0) + 1;
+        }
+
+        foreach ($brandCounts1 as $brandId => $adsCount) {
+            $limitForBrand = $brandLimits[$brandId] ?? $globalBrandLimit;
+            $this->assertLessThanOrEqual($limitForBrand, $adsCount);
+        }
+
+        foreach ($brandCounts2 as $brandId => $adsCount) {
+            $limitForBrand = $brandLimits[$brandId] ?? $globalBrandLimit;
+            $this->assertLessThanOrEqual($limitForBrand, $adsCount);
+        }
+    }
+
 }
